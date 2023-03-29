1d692466f27db009358c7f930717d1d92b9db491
Ricardo_Home <ricardo.macias1755@alumnos.udg.mx>
2023-03-29 11:39:37 -0600

    Se crea la funcion de Memory_Space

--- Practica_5.py
+++ Practica_5.py
@@ -4,6 +4,14 @@ class Memory_Management:
         self.memory_space = ['1000kb', '400kb', '1800kb','700kb', '900kb', '1200kb', '1500kb']
         self.file = file
 
+    def Memory_Space(self, kb):
+        kilobytes = ''
+        for count_kb in kb:
+            if count_kb == 'k':
+                return int(kilobytes)
+            else:
+                kilobytes = kilobytes + count_kb
+
 
 def Read_File():
     with open("archivos.txt", "r") as file:
@@ -12,4 +20,6 @@ def Read_File():
 
 
 files = Read_File()
-print(files)
+memory = Memory_Management(files)
+
+print(memory.Memory_Space("1000kb"))
