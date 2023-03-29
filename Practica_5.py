3129369ccc7725675903b954a142a642605aaac6
Ricardo_Home <ricardo.macias1755@alumnos.udg.mx>
2023-03-29 11:37:13 -0600

    Se crea la clase Memory_Management

--- Practica_5.py
+++ Practica_5.py
@@ -1,4 +1,8 @@
 
+class Memory_Management:
+    def __init__(self, file):
+        self.memory_space = ['1000kb', '400kb', '1800kb','700kb', '900kb', '1200kb', '1500kb']
+        self.file = file
 
 
 def Read_File():
